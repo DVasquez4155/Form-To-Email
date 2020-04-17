fec31a9066f90a0ccaa550f68ea4f4cec5cd5e98
Daniel V <main@dvasquez4155.com>
2020-04-17 13:32:57 -0700

    Added CORS

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const moment = require('moment');
 // This sets up the basic properties for our express server
 // ==============================================================================
 
-// Tells node that we are creating an "express" server
-var app = express();
+var cors = require('cors')
+
+var app = express()
+app.use(cors())
 
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
@@ -19,12 +21,12 @@ app.use(express.json());
 app.post("/", function(req,res) {
     console.log(req.body)
     mail({
-            name : req.body.name,
-            email: req.body.email,
-            subject: req.body.subject,
-            message: req.body.message,
-            time: moment().format('LLLL')
-        })
+        name : req.body.name,
+        email: req.body.email,
+        subject: req.body.subject,
+        message: req.body.message,
+        time: moment().format('LLLL')
+    })
     res.send(req.body)
 })
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
